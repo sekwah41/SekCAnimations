caa98b7981e4d4380c8eb0ec32b2c7ea041a1832
Alastair <alastair.hawkes@gmail.com>
2016-10-18 04:16:40 +0100

    renamed a file

--- src/main/java/sekcanimations/api/SekCAnimSimpleApi.java
+++ src/main/java/sekcanimations/api/SekCAnimSimpleApi.java
@@ -3,19 +3,23 @@ package sekcanimations.api;
 import cpw.mods.fml.common.Loader;
 
 /**
+ *
+ * NOTE This class should be used to access stuff for compatability and/or very basic features. It +
+ *
+ *
  * Took the idea of this from seeing iChun do something similar, thanks bud ;)
  *
  * If you want to add support for any mod using anims just copy this file into your mod and use it to communicate without
  * needing a dependency.
  *
- * Also use Loader.isModLoaded("sekcanims") or the SekCAnimApi.isloaded() and dont just keep calling the methods taking advantage
+ * Also use Loader.isModLoaded("sekcanims") or the SekCAnimSimpleApi.isloaded() and dont just keep calling the methods taking advantage
  * of the fact it still works.
  *
  *
  * Would have been called Api but if there is another class called wanting to be used it would start to look messy to use both.
  * Basically the name is too generic.
   */
-public final class SekCAnimApi {
+public final class SekCAnimSimpleApi {
 
     public static boolean isLoaded(){
         return Loader.isModLoaded("sekcanims");
